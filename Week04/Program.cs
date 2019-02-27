71f5407b9ad8c802f22733df1bca8a24cf4795c6
User <User@DEV-022775.OnBase.net>
2019-02-26 19:25:40 -0500

    wek 4 cod alon

---
+++ Week04/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Week04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // bool snowing = true;
+            // bool haveCarrot = true;
+            // int temp = 40;
+            // string clothes = "outfit";
+            // string name = Console.ReadLine();
+
+            // if ((snowing && temp <= 40) && !(name != "Bob" || haveCarrot )) {
+            //     Console.WriteLine(" i  a m  s n o w m a n  x d ");
+            // }
+
+     if ((true != false)) {
+         Console.WriteLine("b i g  b o i");
+     }
+     
+     else {
+         Console.WriteLine("c h e e s e");
+     } 
+
+        }
+    }
+}
