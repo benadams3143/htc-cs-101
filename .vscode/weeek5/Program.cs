118f13153d1b89eb9e329f16b330d1c278f7817a
User <User@DEV-022775.OnBase.net>
2019-03-05 19:26:33 -0500

    eeeeeehhhhhh wek 5 cwap

---
+++ .vscode/weeek5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace weeek5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("numero uno plz");
+            int numerouno = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("numero deus plz");
+            int numerodeus = Convert.ToInt32(Console.ReadLine());
+            
+
+            Console.WriteLine("operator how can i help you");
+            string operatIon = Console.ReadLine();
+
+            if(operatIon == "+")
+            {
+                Console.WriteLine(AddNumbers(numerouno, numerodeus));
+            }
+
+            else if(operatIon == "-")
+            {
+                int diff = SubtractNumbers(numerouno, numerodeus);
+                Console.WriteLine(diff);
+            }
+
+            else
+            {
+                Console.WriteLine("error 404 lol gotti");
+            }
+        }
+
+        public static int AddNumbers(int numerouno, int  numerodeus)
+        {
+            return numerouno + numerodeus;
+        }
+
+        public static int SubtractNumbers(int cheeze, int chese)
+        {
+            return cheeze - chese;
+        }
+    }   
+}
